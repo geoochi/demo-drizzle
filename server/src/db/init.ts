fc04c1b5760beaf3a336efed73069d4655b954e8
Geoo Chi <geoochi@geoochi.com>
2025-04-12 15:33:55 +0800

    update

--- server/src/db/init.ts
+++ server/src/db/init.ts
@@ -7,7 +7,7 @@ const db = drizzle(process.env.DB_FILE_NAME!)
 async function main() {
   const user: typeof kvTable.$inferInsert = { key: 'count', value: '0' }
   await db.insert(kvTable).values(user)
-  console.log('kv table created!')
+  console.log('kv table initialized!')
 }
 
 main()
