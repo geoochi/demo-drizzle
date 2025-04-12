a91303644c876659cca0b69989ef283dfad48c80
Geoo Chi <geoochi@geoochi.com>
2025-04-12 15:55:43 +0800

    use hono

--- server/src/db/init.ts
+++ server/src/db/init.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/libsql'
+import { eq } from 'drizzle-orm'
 import { kvTable } from './schema'
 
 const db = drizzle(process.env.DB_FILE_NAME!)
 
 async function main() {
-  const user: typeof kvTable.$inferInsert = { key: 'count', value: '0' }
-  await db.insert(kvTable).values(user)
-  console.log('kv table initialized!')
+  const rows = await db.select().from(kvTable).where(eq(kvTable.key, 'count'))
+  if (rows.length > 0) {
+    await db.update(kvTable).set({ value: '0' }).where(eq(kvTable.key, 'count'))
+    console.log('count reset!')
+  } else {
+    const row = { key: 'count', value: '0' }
+    await db.insert(kvTable).values(row)
+    console.log('count initialized!')
+  }
 }
 
 main()
--- server/src/server-demo-drizzle.ts
+++ server/src/server-demo-drizzle.ts
@@ -1,33 +1,28 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/libsql'
-import express from 'express'
+import { Hono } from 'hono'
 import { eq } from 'drizzle-orm'
 import { kvTable } from './db/schema'
+import { serve } from '@hono/node-server'
 
 const db = drizzle(process.env.DB_FILE_NAME!)
+const app = new Hono()
 
-const app = express()
-
-app.get('/api/count', async (req, res) => {
+app.get('/api/count', async c => {
   const row = await db.select().from(kvTable).where(eq(kvTable.key, 'count'))
-  res.json({ message: 'ok', count: row[0].value })
+  return c.json({ message: 'ok', count: row[0].value })
 })
 
-app.get('/api/count-plus', async (req, res) => {
+app.get('/api/count-plus', async c => {
   const row = await db.select().from(kvTable).where(eq(kvTable.key, 'count'))
   const count = parseInt(row[0].value)
   await db
     .update(kvTable)
     .set({ value: (count + 1).toString() })
     .where(eq(kvTable.key, 'count'))
-  res.json({ message: 'ok', count: count + 1 })
+  return c.json({ message: 'ok', count: count + 1 })
 })
 
-// 404
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
-app.listen(3003, () => {
-  console.log('Server is running on http://localhost:3003')
+serve({ port: 3000, fetch: app.fetch }, () => {
+  console.log(`Server is running on http://localhost:3000`)
 })
